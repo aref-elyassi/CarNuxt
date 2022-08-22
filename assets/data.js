44f36cb251c320d3b8f064fa983e3eb17f5b7af0
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-22 19:09:09 +0430

    add price to data.js

--- assets/data.js
+++ assets/data.js
@@ -12,28 +12,32 @@ export const smallCardHome=
             id:1,
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
             time:'5 hours ago',
-            image:'car10.png'
+            image:'car10.png',
+            price:'45000$'
     
         },
         {
             id:2,
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
             time:'5 hours ago',
-            image:'car6.png'
+            image:'car6.png',
+            price:'55000$'
     
         },
         {
             id:3,
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
             time:'5 hours ago',
-            image:'car7.png'
+            image:'car7.png',
+            price:'65000$'
     
         },
         {
             id:4,
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
             time:'5 hours ago',
-            image:'car9.png'
+            image:'car9.png',
+            price:'35000$'
     
         }
     ]
@@ -43,7 +47,8 @@ export const smallCardHome=
         id:1,
         description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
         time:'5 hours ago',
-        image:'bike1.png'
+        image:'bike1.png',
+       
     
     }
     export const smallCardHomeBike=
@@ -52,28 +57,32 @@ export const smallCardHome=
                 id:1,
                 description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
                 time:'5 hours ago',
-                image:'bike9.jpg'
+                image:'bike9.jpg',
+                price:'15000$'
         
             },
             {
                 id:2,
                 description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
                 time:'5 hours ago',
-                image:'bike8.jpg'
+                image:'bike8.jpg',
+                price:'5000$'
         
             },
             {
                 id:3,
                 description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
                 time:'5 hours ago',
-                image:'bike7.jpg'
+                image:'bike7.jpg',
+                price:'15000$'
         
             },
             {
                 id:4,
                 description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
                 time:'5 hours ago',
-                image:'bike4.jpg'
+                image:'bike4.jpg',
+                price:'10000$'
         
             }
         ]
