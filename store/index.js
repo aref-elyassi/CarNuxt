da7b35a9e8d7d34a4361665a003c57aedc009a04
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-23 12:19:16 +0430

    add data to store

---
+++ store/index.js
@@ -0,0 +1,148 @@
+export const state=()=>{
+    cars=[
+        {
+            id:1,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car1.jpeg',
+            price:'5000$'
+    
+        },
+        {
+            id:2,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car2.jpg',
+            price:'5000$'
+    
+        },
+        {
+            id:3,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car3.jpg',
+            price:'5000$'
+    
+        },
+        {
+            id:4,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car4.jpeg',
+            price:'5000$'
+    
+        },
+        {
+            id:5,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car5.png',
+            price:'5000$'
+    
+        },
+        {
+            id:6,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car6.png',
+            price:'5000$'
+    
+        },
+        {
+            id:7,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car7.png',
+            price:'5000$'
+    
+        },
+        {
+            id:8,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car8.jpeg',
+            price:'5000$'
+    
+        },
+        {
+            id:9,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car9.png',
+            price:'5000$'
+    
+        },
+        {
+            id:10,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car10.png',
+            price:'5000$'
+    
+        },
+        {
+            id:11,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car11.png',
+            price:'5000$'
+    
+        },
+        {
+            id:12,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car12.png',
+            price:'5000$'
+    
+        },
+        {
+            id:13,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car13.png',
+            price:'5000$'
+    
+        },
+        {
+            id:14,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car14.jpg',
+            price:'5000$'
+    
+        },
+        {
+            id:15,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car15.png',
+            price:'5000$'
+    
+        },
+        {
+            id:16,
+            name:'Lorem',
+            description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            time:'5 hours ago',
+            image:'car16.jpeg',
+            price:'5000$'
+    
+        },
+    ]
+}
