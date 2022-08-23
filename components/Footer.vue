e2674f164270435af54f358b6b29dae8797cb2bf
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-23 11:07:14 +0430

    add some text to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -27,6 +27,136 @@
                         </li>
                     </ul>
                 </div>
+                 <div class="carandbike">
+                    <ul>
+                        <p>Popular Car Brands</p>
+                        <li>
+                            <nuxt-link to="/">Maruti Suzuki</nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/">Hyundai</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Tata</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Volkswagen</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Honda</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Mahindra</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Kia</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+                         <div class="popularbrand">
+                    <ul>
+                        <p>Popular Car Brands</p>
+                        <li>
+                            <nuxt-link to="/">Maruti Suzuki</nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/">Hyundai</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Tata</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Volkswagen</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Honda</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Mahindra</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Kia</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+                    <div class="popularmodel">
+                    <ul>
+                        <p>Popular Car Models</p>
+                        <li>
+                            <nuxt-link to="/">Mahindra XUV 700</nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/">Kia Seltos</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Mahindra Thar</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Tata Nexon</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Kia Sonet</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Tata Punch</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">MG Hector</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+                 <div class="popularmodel">
+                    <ul>
+                        <p>Popular Bike Brands</p>
+                        <li>
+                            <nuxt-link to="/">Royal Enfield</nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/">Honda</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">KTM</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Bajaj</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Yamaha</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">TVS</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Hero</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+                   <div class="popularmodel">
+                    <ul>
+                        <p>Popular Bike Models</p>
+                        <li>
+                            <nuxt-link to="/">Royal Enfield Classic 350</nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/">Yamaha MT-15</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Bajaj Pulsar 220</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">KTM RC 200</nuxt-link>
+                        </li>
+                         <li>
+                            <nuxt-link to="/">Hero Splendor Plus</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Bajaj Pulsar 150</nuxt-link>
+                        </li>
+                          <li>
+                            <nuxt-link to="/">Honda CB Shine</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
             </Footer>
     </div>
 </template>
