a52be85e0cdf40977984b1e536d2147279a12f74
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-23 11:10:41 +0430

    add routerclass

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,25 +5,25 @@
                     <ul>
                         <p>Car&Bike</p>
                         <li>
-                            <nuxt-link to="/">About Us</nuxt-link>
+                            <nuxt-link class="router" to="/">About Us</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">New Cars</nuxt-link>
+                            <nuxt-link class="router" to="/">New Cars</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Used Cars</nuxt-link>
+                            <nuxt-link class="router" to="/">Used Cars</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">New Bikes</nuxt-link>
+                            <nuxt-link class="router" to="/">New Bikes</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Used Bikes</nuxt-link>
+                            <nuxt-link class="router" to="/">Used Bikes</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Sell Your Cars</nuxt-link>
+                            <nuxt-link class="router" to="/">Sell Your Cars</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Privacy Policy</nuxt-link>
+                            <nuxt-link class="router" to="/">Privacy Policy</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -31,25 +31,25 @@
                     <ul>
                         <p>Popular Car Brands</p>
                         <li>
-                            <nuxt-link to="/">Maruti Suzuki</nuxt-link>
+                            <nuxt-link class="router" to="/">Maruti Suzuki</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">Hyundai</nuxt-link>
+                            <nuxt-link class="router" to="/">Hyundai</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Tata</nuxt-link>
+                            <nuxt-link class="router" to="/">Tata</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Volkswagen</nuxt-link>
+                            <nuxt-link class="router" to="/">Volkswagen</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Honda</nuxt-link>
+                            <nuxt-link class="router" to="/">Honda</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Mahindra</nuxt-link>
+                            <nuxt-link class="router" to="/">Mahindra</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Kia</nuxt-link>
+                            <nuxt-link class="router" to="/">Kia</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -57,25 +57,25 @@
                     <ul>
                         <p>Popular Car Brands</p>
                         <li>
-                            <nuxt-link to="/">Maruti Suzuki</nuxt-link>
+                            <nuxt-link class="router" to="/">Maruti Suzuki</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">Hyundai</nuxt-link>
+                            <nuxt-link class="router" to="/">Hyundai</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Tata</nuxt-link>
+                            <nuxt-link class="router" to="/">Tata</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Volkswagen</nuxt-link>
+                            <nuxt-link class="router" to="/">Volkswagen</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Honda</nuxt-link>
+                            <nuxt-link class="router" to="/">Honda</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Mahindra</nuxt-link>
+                            <nuxt-link class="router" to="/">Mahindra</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Kia</nuxt-link>
+                            <nuxt-link class="router" to="/">Kia</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -83,25 +83,25 @@
                     <ul>
                         <p>Popular Car Models</p>
                         <li>
-                            <nuxt-link to="/">Mahindra XUV 700</nuxt-link>
+                            <nuxt-link class="router" to="/">Mahindra XUV 700</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">Kia Seltos</nuxt-link>
+                            <nuxt-link class="router" to="/">Kia Seltos</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Mahindra Thar</nuxt-link>
+                            <nuxt-link class="router" to="/">Mahindra Thar</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Tata Nexon</nuxt-link>
+                            <nuxt-link class="router" to="/">Tata Nexon</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Kia Sonet</nuxt-link>
+                            <nuxt-link class="router" to="/">Kia Sonet</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Tata Punch</nuxt-link>
+                            <nuxt-link class="router" to="/">Tata Punch</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">MG Hector</nuxt-link>
+                            <nuxt-link class="router" to="/">MG Hector</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -109,25 +109,25 @@
                     <ul>
                         <p>Popular Bike Brands</p>
                         <li>
-                            <nuxt-link to="/">Royal Enfield</nuxt-link>
+                            <nuxt-link class="router" to="/">Royal Enfield</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">Honda</nuxt-link>
+                            <nuxt-link class="router" to="/">Honda</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">KTM</nuxt-link>
+                            <nuxt-link class="router" to="/">KTM</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Bajaj</nuxt-link>
+                            <nuxt-link class="router" to="/">Bajaj</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Yamaha</nuxt-link>
+                            <nuxt-link class="router" to="/">Yamaha</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">TVS</nuxt-link>
+                            <nuxt-link class="router" to="/">TVS</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Hero</nuxt-link>
+                            <nuxt-link class="router" to="/">Hero</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -135,26 +135,31 @@
                     <ul>
                         <p>Popular Bike Models</p>
                         <li>
-                            <nuxt-link to="/">Royal Enfield Classic 350</nuxt-link>
+                            <nuxt-link class="router" to="/">Royal Enfield Classic 350</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link to="/">Yamaha MT-15</nuxt-link>
+                            <nuxt-link class="router" to="/">Yamaha MT-15</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Bajaj Pulsar 220</nuxt-link>
+                            <nuxt-link class="router" to="/">Bajaj Pulsar 220</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">KTM RC 200</nuxt-link>
+                            <nuxt-link class="router" to="/">KTM RC 200</nuxt-link>
                         </li>
                          <li>
-                            <nuxt-link to="/">Hero Splendor Plus</nuxt-link>
+                            <nuxt-link class="router" to="/">Hero Splendor Plus</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Bajaj Pulsar 150</nuxt-link>
+                            <nuxt-link class="router" to="/">Bajaj Pulsar 150</nuxt-link>
                         </li>
                           <li>
-                            <nuxt-link to="/">Honda CB Shine</nuxt-link>
+                            <nuxt-link class="router" to="/">Honda CB Shine</nuxt-link>
                         </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+
                     </ul>
                 </div>
             </Footer>
@@ -167,6 +172,17 @@
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style  scoped>
+.router{
+    color: #949393;
+    text-decoration: none;
+    width: 11rem;
+    font-size: 1.5rem;
+    margin-top: 3rem;
+   
+}
+.router:hover{
+    text-decoration: underline;
+    color: rgb(255, 230, 4);
+}
 </style>
