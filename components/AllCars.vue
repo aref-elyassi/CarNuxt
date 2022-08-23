51cdf66a7f67467466fb2e199b7230d07fdfc776
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-23 12:43:56 +0430

    add some change and add carsdisplay

---
+++ components/AllCars.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="allcars">
+            <div class="content-all-cars">
+            <img :src="require(`@/assets/images/${allcar.image}`)" alt="">
+            <h3>{{allcar.name}}</h3>
+            </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['allcar']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/cars/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <AllCars
+            v-for="cars in product" :key="cars.id"
+        :allcar="cars"/>
+</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import AllCars from '../../components/AllCars.vue';
+    export default {
+    computed: {
+        product() {
+            return this.$store.getters.allCars;
+        }
+    },
+    components: { AllCars }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
