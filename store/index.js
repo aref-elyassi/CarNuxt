645b61279b89ed382449201353428563de0df3cf
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-23 12:26:52 +0430

    add getters

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
-export const state=()=>{
-    cars=[
+export const state=()=>({
+    cars:[
         {
             id:1,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car1.jpeg',
             price:'5000$'
@@ -13,6 +15,7 @@ export const state=()=>{
             id:2,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
             time:'5 hours ago',
             image:'car2.jpg',
             price:'5000$'
@@ -22,6 +25,8 @@ export const state=()=>{
             id:3,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car3.jpg',
             price:'5000$'
@@ -31,6 +36,8 @@ export const state=()=>{
             id:4,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car4.jpeg',
             price:'5000$'
@@ -40,6 +47,8 @@ export const state=()=>{
             id:5,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car5.png',
             price:'5000$'
@@ -49,6 +58,8 @@ export const state=()=>{
             id:6,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car6.png',
             price:'5000$'
@@ -58,6 +69,8 @@ export const state=()=>{
             id:7,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car7.png',
             price:'5000$'
@@ -67,6 +80,8 @@ export const state=()=>{
             id:8,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car8.jpeg',
             price:'5000$'
@@ -76,6 +91,8 @@ export const state=()=>{
             id:9,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car9.png',
             price:'5000$'
@@ -85,6 +102,8 @@ export const state=()=>{
             id:10,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car10.png',
             price:'5000$'
@@ -94,6 +113,8 @@ export const state=()=>{
             id:11,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car11.png',
             price:'5000$'
@@ -103,6 +124,8 @@ export const state=()=>{
             id:12,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car12.png',
             price:'5000$'
@@ -112,6 +135,8 @@ export const state=()=>{
             id:13,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car13.png',
             price:'5000$'
@@ -121,6 +146,8 @@ export const state=()=>{
             id:14,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car14.jpg',
             price:'5000$'
@@ -130,6 +157,8 @@ export const state=()=>{
             id:15,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car15.png',
             price:'5000$'
@@ -139,10 +168,18 @@ export const state=()=>{
             id:16,
             name:'Lorem',
             description:'2022 Porsche 911 GT3 RS Debuts As A Road-Legal Monster',
+            description2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ullam saepe facere ea at. Debitis, atque tempora! Enim aspernatur sed facilis repudiandae voluptatem ea itaque deleniti ut aliquam, odio delectus!',
+
             time:'5 hours ago',
             image:'car16.jpeg',
             price:'5000$'
     
         },
     ]
+})
+
+export const getters={
+    allCars(state){
+        return state.cars
+    }
 }
